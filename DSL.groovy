9a2bf0816176c72ebcc6d9b0f4b9273228a226af
Eshwar <87220369+Eshwar-Nayini@users.noreply.github.com>
2022-05-19 09:14:06 +0530

    Update DSL.groovy

--- DSL.groovy
+++ DSL.groovy
@@ -1,28 +1,5 @@
-#!groovy
-windowsNode = 'windows'
-
-pipeline {
-  agent none
-  stages {
-    stage('Stage 1') {
-      agent {
-        label windowsNode
-      }
-      steps {
-        script {
-          // all subsequent steps should be run on the same windows node
-          windowsNode = NODE_NAME
-        }
-        echo "windowsNode: $windowsNode, NODE_NAME: $NODE_NAME"
-      }
-    }
-    stage('Stage 2') {
-      agent {
-        label windowsNode
-      }
-      steps {
-        echo "windowsNode: $windowsNode, NODE_NAME: $NODE_NAME"
-      }
-    }
+job('example') {
+  steps {
+    batchFile('echo Hello World!')
   }
 }
